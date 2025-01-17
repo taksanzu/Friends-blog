2025316131c9c228883f10a2ab8c2a4eafb5cb77
Taksanzu <ankhang897979@gmail.com>
2025-01-17 14:40:47 +0700

    add style and scripts

--- wp-content/themes/friends-blod-theme/functions.php
+++ wp-content/themes/friends-blod-theme/functions.php
@@ -1,7 +1,23 @@
 <?php
 
 function friends_blog_files() {
+    //Styles
+
+    //main style
     wp_enqueue_style('friends_blog_main_styles', get_stylesheet_uri());
+    //boostrap
+    wp_enqueue_style('bootstrap', '//cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css');
+    //fontawesome
+    wp_enqueue_style('fontawesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
+
+    //Scripts
+
+    //jquery
+    wp_enqueue_script('jquery', '//cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js');
+    //bootstrap
+    wp_enqueue_script('bootstrap', '//cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.min.js');
+
+
 }
 
 add_action('wp_enqueue_scripts', 'friends_blog_files');
--- wp-content/themes/friends-blod-theme/header.php
+++ wp-content/themes/friends-blod-theme/header.php
@@ -1,8 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php wp_head(); ?>
     <title>Friends Blog</title>
 </head>
+
 <body>
-    
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="<?php echo home_url() ?>">Home <span
+                                class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main class="container">
+        <h1>Friends Blog</h1>
