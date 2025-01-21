e5bd97077cd52f710bc40e7ad3a93386464605bd
Taksanzu <ankhang897979@gmail.com>
2025-01-21 15:16:08 +0700

    add conditional for child pages

--- wp-content/themes/friends-blod-theme/page.php
+++ wp-content/themes/friends-blod-theme/page.php
@@ -4,23 +4,30 @@
 
   while(have_posts()) {
     the_post(); ?>
-    
-    <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+
+<div class="page-banner">
+    <div class="page-banner__bg-image"
+        style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>DONT FORGET TO REPLACE ME LATER</p>
-      </div>
-    </div>  
-  </div>
-
-  <div class="container container--narrow page-section">
-
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+            <p>DONT FORGET TO REPLACE ME LATER</p>
+        </div>
     </div>
-    
+</div>
+
+<div class="container container--narrow page-section">
+
+    <?php
+      $theParent = wp_get_post_parent_id(get_the_ID());
+      if ($theParent) { ?>
+    <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>"><i class="fa fa-home"
+                    aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> <span
+                class="metabox__main"><?php echo the_title() ?></span></p>
+    </div>
+    <?php } ?>
+
     <!--
     <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
@@ -32,12 +39,12 @@
     -->
 
     <div class="generic-content">
-      <?php the_content(); ?>
+        <?php the_content(); ?>
     </div>
 
-  </div>
-    
-  <?php }
+</div>
+
+<?php }
 
   get_footer();
 
