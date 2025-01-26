406c2cc778395cdff1665d574ed29cac6d4d6e67
Taksanzu <ankhang897979@gmail.com>
2025-01-26 16:13:21 +0700

    update event editor and event home page

--- wp-content/mu-plugins/friends-blog-post-types.php
+++ wp-content/mu-plugins/friends-blog-post-types.php
@@ -2,6 +2,8 @@
 
 function friends_blog_plugin(){
     register_post_type('event', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
         'public' => true,
